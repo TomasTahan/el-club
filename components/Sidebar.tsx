0a22e2c49f2ee9d1697668cca74ca2f5d38b7d9b
Tomas <jtomastahan@gmail.com>
2024-04-28 15:27:59 -0400

    test2

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,7 +5,7 @@ export default function Sidebar() {
   return (
     <div className="space-y-4 flex flex-col h-full text-primary border-r border-black/25">
       <div className="px-2 py-10">
-        <p>Las Pircas</p>
+        <p>El Club</p>
 
         <div className="flex flex-col gap-10 mt-20 justify-center items-center">
           <Link href="/" className="group">
